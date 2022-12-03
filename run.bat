d8086fe193e2a4bcee737daf566a4aa2d1f24533
Joël Kuijper <joel@deindruk.nl>
2022-12-03 14:37:44 +0100

    feat: working template

---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Running plugin.exe
+".\dist\plugin.exe" %*
+
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello world");
+import { open } from "https://deno.land/x/opener@v1.0.1/mod.ts";
+import { Flow } from "./flow-launcher-helper.ts";
+
+const { params, on, showResult } = new Flow("assets/app.png");
+
+showResult({
+  title: "Hello World Typescript",
+  subtitle: "Showing your query parameters: " + params + ". Click to open Flow's website",
+  method: "do_something_for_query",
+  iconPath: "assets/app.png",
+  score: 0,
+});
+
+on("do_something_for_query", () => {
+  const url = params;
+
+  open(url);
+});
